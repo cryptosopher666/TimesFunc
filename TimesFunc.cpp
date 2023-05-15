0e134cf43fcf6a1e02f83d20adddc32a11500c65
Mohammad <m.mirgolbabaei666@gmail.com>
2023-05-15 18:01:24 +0330

    Create TimesFunc.cpp

---
+++ TimesFunc.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int times(int,int);
+
+int main(){
+	int a , b;
+	cout<<"Enter two numbers: "<<endl;
+	cin>>a>>b;
+	cout<<times(a,b);
+	getch();
+	return 0;
+}
+int times(int a , int b){
+	if(b==1)
+	return a;
+	else
+	return times(a,b-1)+a;
+}
+
+
